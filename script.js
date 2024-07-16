7eedd384a191bf26e20fb468afa97b9a6bf21330
Oksana Mironova <kq3hip13z@gmail.com>
2024-07-16 08:23:22 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+/** ============ex 1================= */
+// Základní datové typy a proměnné
+// Vytvořte proměnné pro název položky, množství a stav zakoupení: itemName, quantity, isBought
+
+/** ============ex 2================= */
+// Objekt
+// Vytvořte objekt item pro položku nákupního seznamu
+
+/** ============ex 3================= */
+// Pole a metody push a pop
+// Vytvořte proměnnou shoppingList s prazdným polem
+// Přidejte objekt do pole shoppingList pomocí metody push.
+// Zobrazte pole v konzoli.
+
+/** ============ex 4================= */
+// Přidání nové položky při kliknutí na tlačítko "Přidat"
+// Přidejte možnost zadání nové položky a přidání do seznamu při kliknutí na tlačítko "Přidat".
+
+/** ============ex 5================= */
+// Odstranění poslední položky při kliknutí na tlačítko "Odstranit poslední"
+
+//Přidejte možnost odstranění poslední položky při kliknutí na tlačítko "Odstranit poslední".
+
+/** ============ex 6================= */
+// Metoda forEach
+
+// Použijte metodu forEach pro zobrazení položek v seznamu na webové stránce.
+
+/** ============ex 7================= */
+// Metoda map
+
+// Použijte metodu map pro vytvoření pole s názvy položek a zobrazte jej v konzoli.
+
+/** ============ex 8================= */
+// Inicializace seznamu při načtení stránky
+updateShoppingList();
